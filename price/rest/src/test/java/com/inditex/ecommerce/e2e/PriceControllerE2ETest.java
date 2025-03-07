03a84be7341cbeeb0156487204d9788d5fa440d8
DESKTOP-3HMLMJH\Alberto <a>
2025-03-07 20:04:58 +0100

    Limpieza de codigo

--- price/rest/src/test/java/com/inditex/ecommerce/e2e/PriceControllerE2ETest.java
+++ price/rest/src/test/java/com/inditex/ecommerce/e2e/PriceControllerE2ETest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest( classes = EcommerceApplication.class,webEnvironment = WebEnvironment.RANDOM_PORT)
-public class PriceControllerE2E {
+public class PriceControllerE2ETest {
 
 	@Autowired
 	private TestRestTemplate testRestTemplate;
